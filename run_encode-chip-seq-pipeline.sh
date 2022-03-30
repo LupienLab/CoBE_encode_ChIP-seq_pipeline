eab786774db730ad6fcacdf04e34aad5b6336b24
nandankita <nandankita@gmail.com>
2022-03-30 00:13:11 -0400

    Update run_encode-chip-seq-pipeline.sh

--- run_encode-chip-seq-pipeline.sh
+++ run_encode-chip-seq-pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load caper/1.4.2
 module load singularity
-sbatch -p all -J chip_submit --export=ALL --mem 3G -t 5-0 --wrap "caper run /PATH/TO/chip.wdl -c /PATH/TO/caper.conf --cromwell /PATH/TO/cromwell-42.jar --womtool /PATH/TO/womtool-52.jar -i $1 --singularity /PATH/TO/singularity/chip-seq-pipeline_v1.7.1.sif"
+sbatch -p all -J chip_submit --export=ALL --mem 3G -t 5-0 --wrap "caper run /PATH/TO/chip.wdl -c /PATH/TO/caper.conf --cromwell /PATH/TO/cromwell-78.jar --womtool /PATH/TO/womtool-78.jar -i $1 --singularity /PATH/TO/singularity/chip-seq-pipeline_v1.7.1.sif"
